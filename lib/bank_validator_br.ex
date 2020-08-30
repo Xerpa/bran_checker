55853b30b30179265f78ee55c7f2b7c39ea20599
Samuel Grave <ssilva@xerpa.com.br>
2020-08-30 14:59:53 -0300

    refactors digit_calculator and bank_validator_br

--- lib/bank_validator_br.ex
+++ lib/bank_validator_br.ex
@@ -1,5 +1,6 @@
 defmodule BankValidatorBR do
-  alias BankValidatorBR.Itau
+  alias BankValidatorBR.Banks.Itau
+  alias BankValidatorBR.Banks.Santander
 
   @moduledoc """
   Documentation for `BankValidatorBR`.
@@ -13,24 +14,30 @@ defmodule BankValidatorBR do
     iex> BankValidatorBR.is_valid?("341","2545", "02366", 1)
     :true
   """
+  def is_valid?(_bank_code, _agency_code, _account_number, digit) when is_nil(digit),
+    do: raise("Digit is null")
+
   @spec is_valid?(String.t(), String.t(), String.t(), Integer.t()) :: Boolean.t()
   def is_valid?(bank_code, agency_code, account_number, digit) do
-    parsed_agency_code =
-      agency_code
-      |> String.codepoints()
-      |> Enum.map(&String.to_integer/1)
+    parsed_agency_code = parse_to_integer_list(agency_code)
 
-    parsed_account_number =
-      account_number
-      |> String.codepoints()
-      |> Enum.map(&String.to_integer/1)
+    parsed_account_number = parse_to_integer_list(account_number)
 
     case bank_code do
+      "033" ->
+        Santander.is_valid?(parsed_agency_code, parsed_account_number, digit)
+
       "341" ->
         Itau.is_valid?(parsed_agency_code, parsed_account_number, digit)
 
       _ ->
         raise "Bank #{bank_code} is not supported"
     end
+  end
+
+  defp parse_to_integer_list(numbers) do
+    numbers
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
   end
 end
--- lib/digit_calculator.ex
+++ lib/digit_calculator.ex
@@ -1,12 +1,13 @@
 defmodule BankValidatorBR.DigitCalculator do
-  defp calc_numbers(account_number, weights, sum_result_when_greater_than_9) do
+  @spec calc_numbers([Integer.t()], [Integer.t()], Boolean.t()) :: Integer.t()
+  def calc_numbers(account_number, weights, sum_result_when_greater_than_9 \\ false) do
     account_number
     |> Enum.zip(weights)
-    |> Enum.map(&calc_number(&1, sum_result_when_greater_than_9))
+    |> Enum.map(&calc_number_pair(&1, sum_result_when_greater_than_9))
     |> Enum.sum()
   end
 
-  defp calc_number(numbers, sum_result_when_greater_than_9) do
+  defp calc_number_pair(numbers, sum_result_when_greater_than_9) do
     result = elem(numbers, 0) * elem(numbers, 1)
 
     sum_result(result, sum_result_when_greater_than_9)
@@ -20,6 +21,7 @@ defmodule BankValidatorBR.DigitCalculator do
 
   defp sum_result(result, _sum_result_when_greater_than_9), do: result
 
+  @spec mod([Integer.t()], Integer.t(), [Integer.t()], Boolean.t()) :: Integer.t()
   def mod(full_account_number, mod_factor, weights, sum_result_when_greater_than_9 \\ false),
     do:
       mod_factor -
