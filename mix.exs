e4972c3f440e3bf9c8e6ddf1e449e77cb1264abb
Samuel Grave <ssilva@xerpa.com.br>
2020-08-31 10:05:48 -0300

    changes version for 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BankValidatorBR.MixProject do
     [
       app: :bank_validator_br,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
