324ca36544659405ada42a51420f777bd84d8dc9
Gabriel Rios <gabrielfrios@gmail.com>
2020-09-01 15:43:09 -0300

    change preferred cli env to circle

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule BankValidatorBR.MixProject do
       preferred_cli_env: [
         coveralls: :test,
         "coveralls.detail": :test,
-        "coveralls.github": :test,
+        "coveralls.circle": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
       ],
